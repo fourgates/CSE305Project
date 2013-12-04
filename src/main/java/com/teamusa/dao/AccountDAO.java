2d3ac05f424f0cf2cc82b812b9b6c75af1c8bff0
Phillip Ninan <ninan.phillip@gmail.com>
2013-12-04 01:51:50 -0500

    Revert "mac commit"
    This reverts commit 50b429cbb7fa34da65c9f2d4f5547be8d5ec61a1.

--- src/main/java/com/teamusa/dao/AccountDAO.java
+++ src/main/java/com/teamusa/dao/AccountDAO.java
@@ -1,10 +1,16 @@
 package com.teamusa.dao;
 
 import com.teamusa.model.Account;
-import com.teamusa.model.Customer;
 
-public interface AccountDAO {
+public interface AccountDAO  
+{
+	//Create
 	public void insert(Account customer);
-
+	//Read
 	public Account findByAccountNumber(int accountNumber);
+	//Update
+	public void setAccountStatus(String status);
+	//Delete
+	public void deleteAccount(int accountNumber);
+	
 }
