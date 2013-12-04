ab19c01c0112a8303679dd252d1b548e6f6c150c
samiamorwas <samiamorwas@gmail.com>
2013-12-04 17:28:16 -0500

    Finished insert functionality for all DAOs.

--- src/main/java/com/teamusa/dao/impl/MessageDAO.java
+++ src/main/java/com/teamusa/dao/impl/MessageDAO.java
@@ -1,7 +1,48 @@
 package com.teamusa.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.teamusa.dao.AbstractDAO;
+import com.teamusa.model.Message;
 
 public class MessageDAO extends AbstractDAO {
+	
+	public void insert(Message message) {
+		
+		//create sql query
+		String sql = this.createInsertString();
+		Connection conn = null;
+		
+		//connect to DB
+		try {
+			conn = dataSource.getConnection();
+			//set statement to sql
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			//set instance variables of customers 
+			ps.setInt(1, message.getMessageID());
+			ps.setDate(2, message.getDate());
+			ps.setString(3, message.getSubject());
+			ps.setString(4, message.getContent());
+			ps.setInt(5, message.getSender());
+			ps.setInt(6, message.getReceiver());
+			
+			//update table with query
+			ps.executeUpdate();
+			ps.close();
+ 
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+ 
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
+	}
 
 }
--- src/main/java/com/teamusa/dao/impl/PurchaseDAO.java
+++ src/main/java/com/teamusa/dao/impl/PurchaseDAO.java
@@ -1,7 +1,48 @@
 package com.teamusa.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.teamusa.dao.AbstractDAO;
+import com.teamusa.model.Purchase;
 
 public class PurchaseDAO extends AbstractDAO {
+	
+	public void insert(Purchase purchase) {
+		
+		//create sql query
+		String sql = this.createInsertString();
+		Connection conn = null;
+		
+		//connect to DB
+		try {
+			conn = dataSource.getConnection();
+			//set statement to sql
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			//set instance variables of customers 
+			ps.setInt(1, purchase.getTransactionID());
+			ps.setDate(2, purchase.getDate());
+			ps.setInt(3, purchase.getAdvertisement());
+			ps.setInt(4, purchase.getNumberOfUnits());
+			ps.setInt(5, purchase.getAccount());
+			ps.setInt(6, purchase.getUser());
+			
+			//update table with query
+			ps.executeUpdate();
+			ps.close();
+ 
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+ 
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
+	}
 
 }
