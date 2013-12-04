6fe14b48585a13591fe5d8434b47e62c6b7672c7
Phillip Ninan <ninan.phillip@gmail.com>
2013-12-04 04:12:57 -0500

    Auth part 2 done created login controller need to add login logic, coming next...

--- src/main/java/com/teamusa/controller/HelloController.java
+++ src/main/java/com/teamusa/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/welcome2")
 public class HelloController 
 {
 	@RequestMapping(method = RequestMethod.GET)
