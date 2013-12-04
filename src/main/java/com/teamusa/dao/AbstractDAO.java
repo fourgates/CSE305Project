80d30e29061fd6b6ad8c05313fb0c19b94f44f72
samiamorwas <samiamorwas@gmail.com>
2013-12-04 17:38:39 -0500

    Added deete functionality.

--- src/main/java/com/teamusa/dao/AbstractDAO.java
+++ src/main/java/com/teamusa/dao/AbstractDAO.java
@@ -1,5 +1,9 @@
 package com.teamusa.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import com.teamusa.util.SqlString;
@@ -18,5 +22,25 @@ public class AbstractDAO {
 		SqlString sqlString = this.sqlUtil.createInsertString(this);
 		String sql = "INSERT INTO " + sqlString.name + " " + sqlString.columns + " VALUES " + sqlString.values;
 		return sql;
+	}
+	
+	protected void delete(String column, String val) {
+		Connection conn = null;
+		try {
+			conn = dataSource.getConnection();
+			SqlString sqlString = this.sqlUtil.createInsertString(this);
+			String sql = "DELETE FROM " + sqlString.name + " WHERE " + column + " = " + val;
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
 	}
 }
